b39e7ef182455b25a41324b3c33d8e6d541919e4
robbridges <robbridges@users.noreply.github.com>
2021-05-13 19:50:17 -0700

    playing with components and state

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,3 +1,24 @@
-export const Child = () => {
-  return <div>Hi there!</div>
+interface ChildProps {
+  color: string;
+  onClick: () => void; 
+}
+
+export const Child = ({color, onClick}: ChildProps) => {
+  return <div>
+    { color }
+    <button onClick={onClick}>
+      Click me
+    </button>
+    </div>
 };
+
+// the below is more useful as we are specifically telling React that this is a function component, and gain access to the properties of Function components.
+export const ChildAsFC: React.FC<ChildProps> = ( { color, onClick }) => {
+  return <div>
+    {color}
+    <button onClick={onClick}>
+      Click me
+    </button>
+    </div>;
+};
+
